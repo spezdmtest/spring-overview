9b336c3f259d1a668f3294488f73980fbc933fec
spezdmtest <spezd.m.@gmail.com>
2024-01-10 16:37:06 +0200

    SpringApplication - Bean Scope

--- src/main/java/com/griddynamics/Scope_Annotation_Configuration/Robot.java
+++
@@ -1,9 +0,0 @@
-package com.griddynamics.Scope_Annotation_Configuration;
-
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Scope("prototype")
-@Component
-public class Robot {
-}
--- src/main/java/com/griddynamics/SpringApplication.java
+++ src/main/java/com/griddynamics/SpringApplication.java
@@ -6,9 +6,6 @@ import com.griddynamics.Scope_Java_Configuration.RobotProductionConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-/**
- * Hello world!
- */
 public class SpringApplication {
     public static void main(String[] args) {
 
@@ -34,15 +31,5 @@ public class SpringApplication {
         System.out.println(robot1);
         Robot robot2 = context.getBean(Robot.class);
         System.out.println(robot2);
-
-        //Annotation_Configuration
-//       ApplicationContext context =
-//                new AnnotationConfigApplicationContext("com.griddynamics.Scope_Annotation_Configuration");
-//        RobotFactory robotFactory = context.getBean(RobotFactory.class);
-//        System.out.println(robotFactory);
-//        Robot robot1 = context.getBean(Robot.class);
-//        System.out.println(robot1);
-//        Robot robot2 = context.getBean(Robot.class);
-//        System.out.println(robot2);
     }
 }
